890b4b43916cc9ace8b3b2d7722cfec8bf97a6ad
养乐多lsj <11275955+yakult-lsj@user.noreply.gitee.com>
2024-09-13 16:11:07 +0800

    init

---
+++ _next/static/yk06XkcHafeWroM1tUuFt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/957d7fef86446e7e.css","static/chunks/pages/index-0fc4b2fac021b990.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
